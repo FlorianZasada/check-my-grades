4dfc21ba39c62b28eccee4fb1d377dce918fcca5
Florian <florian.zasada@gmail.com>
2021-09-28 19:49:06 +0200

    fixfix

--- check_grades.py
+++ check_grades.py
@@ -44,18 +44,21 @@ class grades():
 
         """
 
-        GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-        CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+        # GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+        # CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
         chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument('--disable-gpu')
-        # chrome_options.add_argument('--no-sandbox')
-        chrome_options.binary_location = GOOGLE_CHROME_PATH
+
+        chrome_options = os.environ.get('GOOGLE_CHROME_PATH')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        options.add_argument('--headless')
+        # chrome_options.binary_location = GOOGLE_CHROME_PATH
 
 
         # Öffnen des Browsers sowie den Seiten
-        url = os.environ['QIS_URL']
-        self.driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+        url = os.environ.get('QIS_URL')
+        self.driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
         self.driver.get(url)
 
 
