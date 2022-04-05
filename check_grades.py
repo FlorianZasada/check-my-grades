411c9395399d98f635992b710b4278aca77627de
Florian Zasada <florianzasada@MacBook-Air-von-Florian.local>
2022-04-05 10:18:14 +0200

    fix navigation2

--- check_grades.py
+++ check_grades.py
@@ -119,11 +119,11 @@ class grades():
 
             # Navigieren in die Ordnerstruktur, wo die Noten drinstehen
             try:
-                leistung_btn = self.driver.find_element_by_xpath("""//*[contains(text(), 'Leistungsübersicht')]""").click()
-                #leistung_btn = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="navi-main"]/li[3]/a'))).click()
+                # leistung_btn = self.driver.find_element_by_xpath("//a[contains(text(), 'Leistungsübersicht')]").click()
+                leistung_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(), 'Leistungsübersicht')]"))).click()
                 self._set_state("Navigiere in das Semeseter")
-                #semester = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/form/ul/li/ul/li/ul/li[2]/a[1]'))).click()
-                semester = self.driver.find_element_by_xpath("""//*[contains(text(), '5.      Semester: Wintersemester 2021/22')]""").click()
+                semester = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(), 'Wintersemester 2021/22')]"))).click()
+                #semester = self.driver.find_element_by_xpath("//a[contains(text(), 'Wintersemester 2021/22')]").click()
             except:
                 raise Exception("Fehler bei Navigation in QIS")
     
