ad46ad8814f241da0dcba7820ecb485b4129b07f
FlorianZasada <florian.zasada@gmail.com>
2022-05-17 08:37:04 +0200

    prints

--- check_grades.py
+++ check_grades.py
@@ -118,9 +118,11 @@ class grades():
 
             # Navigieren in die Ordnerstruktur, wo die Noten drinstehen
             try:
+                print("Suche Leistungsbutton")
                 # leistung_btn = self.driver.find_element_by_xpath("//a[contains(text(), 'Leistungsübersicht')]").click()
                 leistung_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Leistungsübersicht"))).click()
                 self._set_state("Navigiere in das Semeseter")
+                print("Suche Semester")
                 semester = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Wintersemester 2021/22"))).click()
                 #semester = self.driver.find_element_by_xpath("//a[contains(text(), 'Wintersemester 2021/22')]").click()
             except Exception as ex:
